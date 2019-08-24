1547f8952b67bd9205f4dd2ca7bf0b5c4bc553ef
tylervanover <yeti.jones@gmail.com>
2019-08-24 09:07:48 -0500

    Changes for nexus

--- src/main/java/com/tvanover/learn/JenkinsBuild.java
+++ src/main/java/com/tvanover/learn/JenkinsBuild.java
@@ -3,6 +3,7 @@ package com.tvanover.learn;
 public class JenkinsBuild {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        System.out.println("I just made a change to test Nexus!");
         System.out.println("Fixed the build!"); // Now it is fixed
     }
 }
